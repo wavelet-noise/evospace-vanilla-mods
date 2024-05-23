6890b342c0734a7abc870889f7c3424bb7fb2a7d
wavelet-noise <ishellstrike@gmail.com>
2024-05-24 02:17:39 +0300

    event system; asset owner

--- Vanilla/init.lua
+++ Vanilla/init.lua
@@ -1,15 +1,34 @@
 local VanillaMod = {}
 
-function VanillaMod.init()
-    for index, proto in pairs(db:objects()) do
-        local block = StaticBlock.cast(proto)
-        if block ~= nil and block.logic ~= nil then
-            if block.logic:is_child_of(AbstractCrafter.get_class()) then
-            end
-        end
+function dump(o)
+    if type(o) == 'table' then
+       local s = '('
+       for k,v in pairs(o) do
+          s = s .. k ..' = ' .. dump(v) .. ','
+       end
+       return s .. ')'
+    elseif type(o) == 'userdata' then
+        return tostring(o)
+    else
+       return tostring(o)
     end
+ end
 
-    
+function VanillaMod.init()
+    local es = EventSystem.get_instance()
+    es:sub(defines.events.on_player_spawn, function(context)
+        print("this is a spawn!")
+        print(dump(context))
+    end)
+    es:sub(defines.events.on_player_mined_item, function(context)
+        print("this is a mine!")
+        print(dump(context))
+    end)
+    es:sub(defines.events.on_built_block, function(context)
+        --dim:set_block_cell(context.pos + Vec3i.new(0,0,1), StaticBlock.find("BasicPlatform"))
+        print("this is a build!")
+        print(dump(context))
+    end)
 end
 
 function VanillaMod.pre_init()
