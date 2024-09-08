c5a5875c81e796c2eff957c0a44a6417981e146e
wavelet-noise <ishellstrike@gmail.com>
2024-09-09 01:16:30 +0300

    Update init.lua

--- Vanilla/init.lua
+++ Vanilla/init.lua
@@ -16,6 +16,23 @@ function dump(o)
 
 function VanillaMod.init()
     local es = EventSystem.get_instance()
+
+    local resources = {
+      {"CopperOre", 1.0},
+      {"IronOre", 0.5},
+      {"CoalOre", 1.5},
+      {"AluminiumOre", 0.25},
+      {"UraniumOre", 0.125},
+      {"MineralWater", 1.0},
+      {"RawOil", 0.5},
+    }
+
+    for key, value in ipairs(resources) do
+      local ed = ExtractionData.new()
+      ed.item = StaticItem.find(value[1])
+      ed.speed = value[2]
+      deposits:add_resource(ed)
+    end
 end
 
 function VanillaMod.pre_init()
