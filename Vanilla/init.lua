c9a2bf1a22acbbed5386cd75d09b18b5270c2f4c
wavelet-noise <ishellstrike@gmail.com>
2024-11-01 15:09:56 +0200

    Update init.lua

--- Vanilla/init.lua
+++ Vanilla/init.lua
@@ -18,12 +18,20 @@ function VanillaMod.init()
    local es = EventSystem.get_instance()
 
    es:sub(defines.events.on_player_at_sector, function(context) 
-      local pos = context.pos * cs.sector_size + Vec3i.new(0,0,23)
-      print("On sector "..tostring(pos)) 
-      local block = StaticBlock.find("CopperMacerator")
-      dim:spawn_block_identity(pos, block)
-      local grid_pos = DeepDeposits.world_block_to_grid(pos)
-      deposits:scan_sector(grid_pos)
+      --local pos = context.pos * cs.sector_size + Vec3i.new(0,0,23)
+      --print("On sector "..tostring(pos)) 
+      --local block = StaticBlock.find("CopperMacerator")
+      --dim:spawn_block_identity(pos, block)
+      local grid_pos = LargeSectors.world_block_to_grid(pos)
+      if not regions:has_region(grid_pos) then
+         local region = regions:get_region(grid_pos)
+
+         local ms = MapStructure.new()
+         ms.structure = ss
+         ms.offset = Vec2i.zero()
+
+         region:add_structure(ms)
+      end
    end)
 
    local resources = {
@@ -42,8 +50,13 @@ function VanillaMod.init()
       local ed = ExtractionData.new()
       ed.item = StaticItem.find(value[1])
       ed.speed = value[2]
-      deposits:add_resource(ed)
+      regions:add_resource(ed)
    end
+
+   local ss = StaticStructure.new("StartPlatform")
+   ss.generate = function(context) print("11111111111") end
+   ss.size = Vec2i.new(10, 10);
+   db:reg(ss)
 end
 
 function VanillaMod.pre_init()
