cbf27edb63ade1ab1cee9b87a6eaf6caa955c08f
wavelet-noise <ishellstrike@gmail.com>
2024-11-23 02:06:29 +0200

    only computations research

--- Vanilla/graphics.lua
+++ Vanilla/graphics.lua
@@ -65,6 +65,44 @@ function register_graphics()
     generate_reflection("GlobalIllumination", "r.DynamicGlobalIlluminationMethod")
     generate_reflection("Reflection", "r.ReflectionMethod")
 
+    db:from_table({
+        class = "Setting",
+        category = "Graphics",
+        type = "String",
+        default_string_value = "90%",
+        string_options = {"30%", "50%", "75%", "85%", "90%", "100%"},
+        action = function(setting)
+            local preset = 90
+            if setting.string_value == "30%" then preset = 30 end
+            if setting.string_value == "50%" then preset = 50 end
+            if setting.string_value == "75%" then preset = 75 end
+            if setting.string_value == "85%" then preset = 85 end
+            if setting.string_value == "100%" then preset = 100 end
+            Console.run("r.ScreenPercentage "..preset)
+        end,
+        label = "ScreenPersentage",
+        name = "ScreenPersentage",
+     })
+
+     db:from_table({
+        class = "Setting",
+        category = "Graphics",
+        type = "String",
+        default_string_value = "TAA",
+        string_options = {"None", "FXAA", "TAA", "TSR"},
+        action = function(setting)
+            local preset = 2
+            if setting.string_value == "None" then preset = 0 end
+            if setting.string_value == "FXAA" then preset = 1 end
+            if setting.string_value == "TSR" then preset = 4 end
+            Console.run("r.AntiAliasingMethod "..preset)
+        end,
+        label = "AntiAliasingMethod",
+        name = "AntiAliasingMethod",
+     })
+
+     
+
     --r.DynamicRes.OperationMode
     --r.AntiAliasingMethod
     --sg.AntiAliasingQuality
